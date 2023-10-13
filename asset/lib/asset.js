d034bd027e06abaddd0fe72cc3f7ce70e6ceb35c
Periklis Kostamis <perikliskostamis@gmail.com>
2023-10-13 20:17:52 +0300

    Get all records of an asset

--- asset/lib/asset.js
+++ asset/lib/asset.js
@@ -17,8 +17,33 @@ class Asset extends Contract {
             : stub.getState(key);
     }
 
+    async _getDataByPartialCompositeKey(stub, asset, collection, ...params) {
+        const results = collection
+            ? await stub.getPrivateDataByPartialCompositeKey(collection, asset, params)
+            : await stub.getStateByPartialCompositeKey(asset, params);
+        return this._parseIterator(results);
+    }
+
+    async _parseIterator(iterator) {
+        const allResults = [];
+        let result = await iterator.next();
+        while (!result.done) {
+            const strValue = Buffer.from(result.value.value.toString()).toString('utf8');
+            let record;
+            try {
+                record = JSON.parse(strValue);
+            } catch (err) {
+                console.log(err);
+                record = strValue;
+            }
+            allResults.push(record);
+            result = await iterator.next();
+        }
+        return stringify(sortKeysRecursive(JSON.parse(allResults)))
+    }
+
     // CreateAsset issues a new asset to the world state with given details.
-    async CreateAsset(ctx, asset, id, data, collection = null) {
+    async CreateAsset(ctx, asset, id, data, collection) {
         if (await this.AssetExists(ctx, asset, id, collection)) {
             throw new Error(`The asset ${asset} with id ${id} already exists`);
         }
@@ -26,7 +51,7 @@ class Asset extends Contract {
         const key = this._createCompositeKey(ctx.stub, asset, id);
         const dataBuffer = Buffer.from(stringify(sortKeysRecursive(JSON.parse(data))));
         // we insert data in alphabetic order using 'json-stringify-deterministic' and 'sort-keys-recursive'
-        collection 
+        collection
             ? await ctx.stub.putPrivateData(collection, key, dataBuffer)
             : await ctx.stub.putState(key, dataBuffer);
         return data;
@@ -34,8 +59,8 @@ class Asset extends Contract {
 
 
     // ReadAsset returns the asset stored in the world state with given id.
-    async ReadAsset(ctx, asset, id, collection = null) {
-        const assetJSON = await this._getData(ctx.stub, asset, collection, id); 
+    async ReadAsset(ctx, asset, id, collection) {
+        const assetJSON = await this._getData(ctx.stub, asset, collection, id);
 
         if (!assetJSON || assetJSON.length === 0) {
             throw new Error(`The asset ${asset} with id ${id} does not exist`);
@@ -45,7 +70,7 @@ class Asset extends Contract {
     }
 
     // UpdateAsset updates an existing asset in the world state with provided parameters.
-    async UpdateAsset(ctx, asset, id, data, collection = null) {
+    async UpdateAsset(ctx, asset, id, data, collection) {
         data = JSON.parse(data);
         const oldAsset = JSON.parse(await this.ReadAsset(ctx, asset, id, collection));
 
@@ -65,25 +90,30 @@ class Asset extends Contract {
     }
 
     // DeleteAsset deletes an given asset from the world state.
-    async DeleteAsset(ctx, asset, id, collection = null) {
+    async DeleteAsset(ctx, asset, id, collection) {
         const exists = await this.AssetExists(ctx, asset, id, collection);
         if (!exists) {
             throw new Error(`The asset ${asset} with id ${id} does not exist`);
         }
 
         const key = this._createCompositeKey(ctx.stub, asset, id);
-        collection 
+        collection
             ? await ctx.stub.deletePrivateData(collection, key)
             : await ctx.stub.deleteState(key);
     }
 
     // AssetExists returns true when asset with given id exists in world state.
-    async AssetExists(ctx, asset, id, collection = null) {
+    async AssetExists(ctx, asset, id, collection) {
         const key = this._createCompositeKey(ctx.stub, asset, id);
         const assetJSON = collection
             ? await ctx.stub.getPrivateData(collection, key)
             : await ctx.stub.getState(key);
         return assetJSON && assetJSON.length > 0;
+    }
+
+    // GetAllAssets returns all records of a specified asset
+    GetAllAssets(ctx, asset, collection) {
+        return this._getDataByPartialCompositeKey(ctx.stub, asset, collection);
     }
 }
 
