18ab981a5437d7871f972c2d3e2cd340f180d801
Periklis Kostamis <perikliskostamis@gmail.com>
2023-10-22 23:40:08 +0300

    [Info] Fix index file

--- info/index.js
+++ info/index.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const Info = require('./lib/info');
+const Asset = require('./lib/asset');
 
 module.exports.Info = Info;
-module.exports.contracts = [Info];
+module.exports.Asset = Asset
+module.exports.contracts = [Info, Asset];
