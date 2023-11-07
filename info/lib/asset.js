27ea0bdc2c06d9c2e6fbec3c111ce191db2c92b5
Periklis Kostamis <perikliskostamis@gmail.com>
2023-11-07 12:55:29 +0200

    Update asset contract in all chaincodes

--- info/lib/asset.js
+++ info/lib/asset.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const _ = require('lodash')
 const stringify = require('json-stringify-deterministic');
 const sortKeysRecursive = require('sort-keys-recursive');
 const { Contract } = require('fabric-contract-api');
@@ -111,6 +112,35 @@ class Asset extends Contract {
             ? await ctx.stub.getPrivateData(collection, key)
             : await ctx.stub.getState(key);
         return assetJSON && assetJSON.length > 0;
+    }
+
+    // Query executes a query against the database and returns the assets that satisfy the specified conditions
+    async Query(ctx, queryString, collection) {
+        const iterator = collection
+            ? (await ctx.stub.getPrivateDataQueryResult(collection, queryString)).iterator
+            : await ctx.stub.getQueryResult(queryString);
+
+        return this._parseIterator(iterator);
+    }
+
+    async validateAsset(ctx, asset, id, target, collection) {
+        target = JSON.parse(target);
+
+        const validationResult = {
+            exists: false,
+            same: false
+        };
+
+        try {
+            const source = JSON.parse(await this.ReadAsset(ctx, asset, id, collection));
+            validationResult.exists = true;
+            validationResult.same = _.isEqual(source, target);
+        } catch (error) {
+            // If ReadAsset throws an error, the asset does not exist.
+            // Exists and same should remain false, so pass
+        }
+
+        return validationResult;
     }
 
     // GetAllAssets returns all records of a specified asset
